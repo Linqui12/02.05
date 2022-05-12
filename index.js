58fc952dfbc6c6041fa5c55d0fc29dc5ae4dfc33
YuriKo <yuri.work95@gmail.com>
2022-05-12 12:07:54 +0300

    CLASS

--- index.js
+++ index.js
@@ -1,36 +1,74 @@
-function checkNumber(userNumber,generateNumber,game,count){
-  if(userNumber==generateNumber){
-    console.log ('You guessed the number');
-    return game = false;
-  } else if (userNumber>generateNumber) {
-    console.log ('Your number more');
-    return game = true;
-  } else if (userNumber<generateNumber) {
-    console.log ('Your number less');
-    
-    return game = true;
-  } else {
-    count--;
-    console.log ('Your number wrong');
-    return game = true;
-  } 
-}
-function guessTheNumber(userNumber){
-  let game = true;
-  const generateNumber=7;
-  let count = 0;
-  while(game){
-    game=checkNumber(userNumber,generateNumber,game,count);
-    if(game===true){
-      count++;
-      userNumber=Number(prompt('enter number :') );
-    }
-  }
-  console.log ('You win ');
-  console.log ('Attempts: '+count);
-}
+//12.05.22
+// const toString = function(){
+//   return `${this.name} ${this.sname}`
+// }
+// const man={
+//   countFingers:5,
+//   read:function(){
+//     return this.name + ' is reading';
+//   }
+// }
+// const student={
+//   id:1,
+//   name: 'Elon',
+//   sname: 'Musk',
+//   age:50,
+//   toString,
+// };
+// student.__proto__=man;//наследование sudent  от man!!!!
+// console.log(student);
+// console.log(student.read());
+// const rabbit={
+  
+//   countPaw : 4,
+//   toJump:function(){
+//     return ' i`M jumping';
+//   }
+// };
+// const rabbitPet={
+//   name:'Dilan',
+//   isPet:function(){
+//     return 'Pet: ' + this.name;
+//   }
+// };
+// const rabbitMagic={
+//   name:'Mike',
+//   ToSay:function(word){
+//     return   this.name + ' say : '+ word;
+//   }
+// };
+// rabbitPet.__proto__= rabbit;
+// rabbitMagic.__proto__=rabbitPet;
+// console.log(rabbitMagic.ToSay('bla'));
 
+// function Ladder(){
+//   this.step=0
+// };
+// function LadderPrototype(){
+//   this.show= function(){
+//     return this.step;
+//   } 
+//   this.up=function(){
+//     this.step++;
+//     return this;
+//   };
+//   this.down=function(){
+//      this.step--;
+//      return this;
+//   };
+// };
+// Ladder.prototype=new LadderPrototype();
 
+// const ladder=new Ladder();
+// console.log(ladder.up());
+// console.log(ladder.show());
+// console.log(ladder.up());
+// console.log(ladder.show());
+// console.log(ladder.down());
+// console.log(ladder.show());
+// console.log(ladder.up().up().up().up().up());
+// console.log(ladder.down().down().up().up().up().show());
+// const a=Math.random();
+// console.log(a);    ------- математический рандом( 0- 10 )
+// console.log(a*10);
 
-
-guessTheNumber(5);
